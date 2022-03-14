8ab7339458b9d999e2e0dd76f24af8f734469603
Piotr-Pietruszka <piopie98@wp.pl>
2022-03-14 16:50:31 +0100

    prediction based on last r samples (no identification)

--- smoothing.py
+++ smoothing.py
@@ -5,6 +5,12 @@ import os
 
 
 def filter(data):
+    r = 4
+    a = np.ones(r)  # parameters of AR model 
+    for n, sample in enumerate(data[r:], r):
+        # prediction for sample n, as linear combination of r previous samples
+        y_pred_n = np.sum(a*data[n-r:n][::-1])  # a[0]*y[n-1] + a[1]*y[n-2] + ...
+
     return 2*data
 
 
