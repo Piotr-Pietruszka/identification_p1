aa85e479ad8c2103a813812d891f7100ca5d8d9f
Piotr-Pietruszka <piopie98@wp.pl>
2022-03-24 22:37:46 +0100

    Now AR identification should work for all files

--- smoothing.py
+++ smoothing.py
@@ -5,12 +5,14 @@ import glob
 import os
 from matplotlib import pyplot as plt
 
+
 def filter(data):
     r = 4 # order of AR model
     theta = np.ones(r)  # vector of parameters of AR model 
-    P = np.ones((r, r), dtype=np.float64)
-    lambda_f = 0.9  # forgetting factor
+    P = np.eye(r, dtype=np.float64)
+    lambda_f = 0.95  # forgetting factor
     smoothed_data = np.copy(data)
+
     for n, sample in enumerate(data[r:], r):
         phi = data[n-r:n][::-1]  # r previous samples
         # prediction for sample n, as linear combination of r previous samples
@@ -18,25 +20,11 @@ def filter(data):
 
         eps = sample-y_pred_n  # prediction error  
 
-        # New parameters
         k = P @ phi / (lambda_f + tr(phi)@P@phi)
-        theta = theta + k*eps
+        theta = theta + k*eps  # New parameters
         P = 1/lambda_f * (P - P@phi@tr(phi)*P / (lambda_f + tr(phi)@P@phi))
 
-        # debug
-        d1 = np.isnan(theta)
-        if True in d1:
-            print(f"theta: {theta}")
-        d2 = np.isnan(phi)
-        if True in d2:
-            print(f"phi: {phi}")
-        # ~~~~~~
-
         smoothed_data[n] = y_pred_n # temp
-
-
-        
-        
 
     return smoothed_data
 
@@ -45,7 +33,7 @@ for filepath in glob.iglob('wav/*.wav'):
     # Read audio data
     samplerate, data = wavfile.read(filepath)
 
-    if filepath not in ['wav\\18.wav']: # debug temp - choose files to process
+    if filepath in []: # debug temp - choose files to process  'wav\\01.wav', 'wav\\02.wav', 'wav\\03.wav', 'wav\\04.wav' ,'wav\\05.wav', 
         print(f"skipp: {filepath}")
         continue
     new_data = filter(data)
