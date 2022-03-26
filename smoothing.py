1b64891f12671c119f295f2c2d5b8651eedc43ce
Piotr-Pietruszka <piopie98@wp.pl>
2022-03-26 17:45:18 +0100

    Smoothing (no 4 samples)

--- smoothing.py
+++ smoothing.py
@@ -6,25 +6,38 @@ import os
 from matplotlib import pyplot as plt
 
 
-def filter(data):
-    r = 4 # order of AR model
+def filter(data, r):
+    r = 4  # order of AR model
     theta = np.ones(r)  # vector of parameters of AR model 
     P = np.eye(r, dtype=np.float64)
     lambda_f = 0.95  # forgetting factor
     smoothed_data = np.copy(data)
+    M = 20
+    last_errors = np.ones(M)*np.max(np.abs(data))  # array of last M errors - to calculate st deviation
+    std_dev = np.sqrt(np.sum(np.square(last_errors))/M)
+    corr_in_row = 0  # number of detected noise samples in a row
 
-    for n, sample in enumerate(data[r:], r):
-        phi = data[n-r:n][::-1]  # r previous samples
+    for n, sample in enumerate(data[r:-1], r):
+        phi = smoothed_data[n-r:n][::-1]  # r previous samples
         # prediction for sample n, as linear combination of r previous samples
         y_pred_n = np.sum(theta*phi)  # a[0]*y[n-1] + a[1]*y[n-2] + ...
 
-        eps = sample-y_pred_n  # prediction error  
+        eps = sample-y_pred_n  # prediction error 
 
-        k = P @ phi / (lambda_f + tr(phi)@P@phi)
-        theta = theta + k*eps  # New parameters
-        P = 1/lambda_f * (P - P@phi@tr(phi)*P / (lambda_f + tr(phi)@P@phi))
+        if np.abs(eps) < 3*std_dev: 
+            # Good sample
+            last_errors[:-1] = last_errors[1:]
+            last_errors[-1] = eps  # shifting errors
+            std_dev = np.sqrt(np.sum(np.square(last_errors))/M)
 
-        smoothed_data[n] = y_pred_n # temp
+            k = P @ phi / (lambda_f + tr(phi)@P@phi)
+            theta = theta + k*eps  # New parameters
+            P = 1/lambda_f * (P - P@phi@tr(phi)*P / (lambda_f + tr(phi)@P@phi))
+            corr_in_row = 0
+        else:
+            # Noise sample
+            smoothed_data[n] = 0.5*(data[n-1]+data[n+1])
+            corr_in_row += 1
 
     return smoothed_data
 
@@ -36,7 +49,7 @@ for filepath in glob.iglob('wav/*.wav'):
     if filepath in []: # debug temp - choose files to process  'wav\\01.wav', 'wav\\02.wav', 'wav\\03.wav', 'wav\\04.wav' ,'wav\\05.wav', 
         print(f"skipp: {filepath}")
         continue
-    new_data = filter(data)
+    new_data = filter(data, 4)
 
     # Write data without impulse noise to file
     basename = os.path.basename(filepath)
@@ -44,10 +57,22 @@ for filepath in glob.iglob('wav/*.wav'):
     wavfile.write(new_filepath, samplerate, new_data)
     print(new_filepath)
 
-    # Plot smoothed
+    plt.subplot(2, 2, 1)
+    plt.plot(data)
+    plt.title("original")
+    plt.subplot(2, 2, 2)
+    plt.plot(new_data)
+    plt.title("smoothed")
+
+    plt.subplot(2, 2, 3)
     plt.plot(data)
     plt.plot(new_data)
     plt.legend(["original", "smoothed"])
+
+    plt.subplot(2, 2, 4)
+    plt.plot(new_data)
+    plt.plot(data)
+    plt.legend(["smoothed", "original"])
     plt.show()
 
 
